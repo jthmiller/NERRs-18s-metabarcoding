c7b353badd3c5d4750d31719a485bb109e5d0fb2
jthmiller <jeffrey.miller@unh.edu>
2025-03-03 16:17:37 -0500

    sdf

--- code/conda-env.sh
+++ code/conda-env.sh
@@ -14,3 +14,15 @@ conda install bioconda::bioconductor-phyloseq
 conda install conda-forge::r-tidyverse
 conda install conda-forge::r-devtools
 
+
+
+
+
+## ##### NEW ENVIRONMENT FOR Qiime2R
+ conda env create -n qiime2-amplicon-2024.5 --file https://data.qiime2.org/distro/amplicon/qiime2-amplicon-2024.5-py39-linux-conda.yml
+ conda activate qiime2-amplicon-2024.5
+ conda install bioconda::bioconductor-phyloseq
+ conda install r-qiime2r
+ conda install bioconda::bioconductor-phyloseq
+ conda install conda-forge::r-tidyverse
+ conda install conda-forge::r-devtools
